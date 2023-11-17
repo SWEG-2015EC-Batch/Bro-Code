fcdce7c3d545dd85f22975a17bd40c36bba7a6bf
LIYUNEH RSTEY <149043085+liyuneh@users.noreply.github.com>
2023-11-17 12:15:02 +0300

    Update x^y.cpp

--- ETS0841_15_Liyuneh_Rstey/Assignment_1/x^y.cpp
+++ ETS0841_15_Liyuneh_Rstey/Assignment_1/x^y.cpp
@@ -1,1 +1,18 @@
 
+// print x raised to y
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    cout<<"enter x \n";
+       double x;
+       cin>>x;
+       cout<<"enter y\n";
+       double y;
+       cin>>y;
+       double z= pow(x, y);
+       cout<<"the value is "<<z;
+       return 0;
+}
