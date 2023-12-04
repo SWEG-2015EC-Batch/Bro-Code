6556e97fe414820105c171a43125038e46591ea1
liyuneh <liyurstey@gmail.com>
2023-12-04 19:01:45 +0300

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,16 @@
 #include<iostream>
 using namespace std;
-void sort( int array[],int size ){
-    int temp;
-    for(int i=0; i<size-1; i++){
-        for(int j=0; j<size-i-1; j++){
-            if(array[j]>array[j+1]){
-                temp=array[j];
-                array[j]=array[j+1];
-                array[j+1]=temp;
-            }
-        }
-    }
-}
-
+class person{
+    public:
+      string name;
+      void display(){
+        cout<<"your name is "<<name<<endl;
+      }
+};
 int main()
 {
-    int array[]= {0,5,2,4,3,1,6,7,9,8,11};
-    int size=sizeof(array)/sizeof(array[0]);
-    sort(array, size);
-    for(int element: array){
-        cout<<element<<" ";
-    }
-   
+    person liyuneh;
+    liyuneh.name="liyuneh";
+    liyuneh.display();
     
-
-    return 0;
 }
