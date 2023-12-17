088793e1b062c81d73528660045aa8f567cb112a
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-17 14:08:06 +0300

    final

--- ETS0823_15_Leulseged_Melaku/Activity 3.1/ex5-number.cpp
+++ ETS0823_15_Leulseged_Melaku/Activity 3.1/ex5-number.cpp
@@ -1,70 +1,198 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
-int main(){
-    int a;
-    cout<<"Ok let's play with numbers please enter any integer =>";
-    cin>>a;
-    int a2 = a;
-    int reverse= 0;
-    int count = 0;
-    int sum = 0;
-    int even = 1;
-    int last = -1;
-    int first;
-    int swap;
-    cout<<"reversed number :";
-    while(a % 10 == 0){
-        cout<<0;
-        a /= 10;
-        count += 1;
-        if (last == -1){
-            last = 0;
-        }
-    }
-    while(a>0){
-        int temp = (a%10);
-        if (last == -1){
-            last = 0;
-            last += temp;
-        //    swap += temp;
-        }
-        reverse = (reverse*10) + temp;
-        a /= 10;
-        count += 1;
-        sum += temp;
-        if (temp % 2 == 0){
-            even *= temp;
-        }
-    }
-    first = reverse % 10;
-    cout<<reverse<<endl;
-    cout<<"there are "<<count<<" digits\n";
-    cout<<"total sum of the digits :"<<sum<<endl;
-    cout<<"total product of the even numbers :"<<even<<endl;
-    cout<<"first number :"<<first<<endl;
-    cout<<"last number :"<<last<<endl;
 
-    int middle;
-    if(count == 1){
-        cout<<"1st and last number swapped :"<<a2<<endl;
-    }else if(count == 2){
-        cout<<"1st and last number swapped :"<<(last * 10) + first<<endl;
-    }
-    else{
-        string a3 = to_string(a2);
-        a3 = a3.substr(1, a3.length() - 2);
-        middle = stoi(a3);
-        swap = (((last * ((count - 2) * 10)) + (middle)) * 10) + first;
-        cout<<"1st and last number swapped :";
+int main() {
+    int number, originalNumber, choice, count, sum, product, first, last, numDigits, temp, swappedNum, rev, original, numDigitsArmstrong, sumArmstrong, sumStrong, originalStrong, sumPerfect;
+    int frequency[10];
 
-        if(last == 0){
-            cout<<"0";
+    cout << "********** Welcome to Number Manipulation **********\n";
+    cout << "Enter an integer: ";
+    cin >> number;
+    originalNumber = number;  // Store the original number
+
+    do {
+        cout << "\n********** Menu **********\n"
+             << "1. Reverse the number\n"
+             << "2. Count the number of digits\n"
+             << "3. Sum of the digits\n"
+             << "4. Product of even digits\n"
+             << "5. Print first and last digit and find their sum\n"
+             << "6. Swap first and last digit\n"
+             << "7. Check if the number is a palindrome\n"
+             << "8. Find if the number is Armstrong\n"
+             << "9. Find if the number is Strong\n"
+             << "10. Check if the number is Perfect\n"
+             << "11. Print digit frequency\n"
+             << "12. Change the number\n"
+             << "0. Exit\n"
+             << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                rev = 0;
+                while (number != 0) {
+                    rev = rev * 10 + number % 10;
+                    number /= 10;
+                }
+                cout << "********** Reversed number: " << rev << " **********\n";
+                break;
+            case 2:
+                cout << "********** Number of digits: ";
+                count = 0;
+                temp = originalNumber;  // Reinitialize temp with the original number
+                while (temp != 0) {
+                    temp /= 10;
+                    count++;
+                }
+                cout << count << " **********\n";
+                break;
+            case 3:
+                sum = 0;
+                temp = number;
+                while (temp != 0) {
+                    sum += temp % 10;
+                    temp /= 10;
+                }
+                cout << "********** Sum of digits: " << sum << " **********\n";
+                break;
+            case 4:
+                product = 1;
+                temp = number;
+                while (temp != 0) {
+                    int digit = temp % 10;
+                    if (digit % 2 == 0) {
+                        product *= digit;
+                    }
+                    temp /= 10;
+                }
+                cout << "********** Product of even digits: " << product << " **********\n";
+                break;
+            case 5:
+                numDigits = log10(number);
+                if (numDigits == 0) {
+                    // For 1-digit numbers, first and last are the same
+                    first = number;
+                    last = number;
+                } else {
+                    last = number % 10;
+                    temp = number / 10;
+                    while (temp >= 10) {
+                        temp /= 10;
+                    }
+                    first = temp;
+                }
+                cout << "********** First digit: " << first << " **********\n";
+                cout << "********** Last digit: " << last << " **********\n";
+                cout << "********** Sum of first and last digit: " << first + last << " **********\n";
+                break;
+            case 6:
+                numDigits = log10(number);
+                if (numDigits == 0) {
+                    // For 1-digit numbers, swapping has no effect
+                    swappedNum = number;
+                } else if (numDigits == 1) {
+                    // For 2-digit numbers, swap first and last
+                    swappedNum = (number % 10) * 10 + number / 10;
+                } else {
+                    last = number % 10;
+                    temp = number / 10;
+                    numDigits = log10(temp);
+                    first = temp / pow(10, numDigits);
+                    temp %= (int)pow(10, numDigits);
+                    swappedNum = last * pow(10, numDigits + 1) + temp * 10 + first;
+                }
+                cout << "********** Number after swapping first and last digit: " << swappedNum << " **********\n";
+                break;
+            case 7:
+                original = number;
+                rev = 0;
+                while (number != 0) {
+                    rev = rev * 10 + number % 10;
+                    number /= 10;
+                }
+                if (original == rev) {
+                    cout << "********** The number is a palindrome **********\n";
+                } else {
+                    cout << "********** The number is not a palindrome **********\n";
+                }
+                break;
+            case 8:
+                original = number;
+                sumArmstrong = 0;
+                numDigitsArmstrong = log10(number);
+                while (number != 0) {
+                    int digit = number % 10;
+                    sumArmstrong += pow(digit, numDigitsArmstrong + 1);
+                    number /= 10;
+                }
+                if (original == sumArmstrong) {
+                    cout << "********** The number is an Armstrong number **********\n";
+                } else {
+                    cout << "********** The number is not an Armstrong number **********\n";
+                }
+                break;
+            case 9:
+                originalStrong = number;
+                sumStrong = 0;
+                temp = number;
+                while (temp != 0) {
+                    int digit = temp % 10;
+                    int factorial = 1;
+                    for (int i = 1; i <= digit; ++i) {
+                        factorial *= i;
+                    }
+                    sumStrong += factorial;
+                    temp /= 10;
+                }
+                if (originalStrong == sumStrong) {
+                    cout << "********** The number is a Strong number **********\n";
+                } else {
+                    cout << "********** The number is not a Strong number **********\n";
+                }
+                break;
+            case 10:
+                sumPerfect = 1;
+                for (int i = 2; i <= number / 2; ++i) {
+                    if (number % i == 0) {
+                        sumPerfect += i;
+                    }
+                }
+                if (sumPerfect == number) {
+                    cout << "********** The number is a Perfect number **********\n";
+                } else {
+                    cout << "********** The number is not a Perfect number **********\n";
+                }
+                break;
+            case 11:
+                for (int i = 0; i < 10; ++i) {
+                    frequency[i] = 0;
+                }
+                temp = originalNumber;
+                while (temp != 0) {
+                    int digit = temp % 10;
+                    frequency[digit]++;
+                    temp /= 10;
+                }
+                cout << "********** Digit Frequency **********\n";
+                for (int i = 0; i < 10; ++i) {
+                    cout << "Digit " << i << ": " << frequency[i] << " times\n";
+                }
+                break;
+            case 12:
+                cout << "********** Enter a new integer: ";
+                cin >> number;
+                originalNumber = number;  // Update the original number
+                break;
+            case 0:
+                cout << "********** Exiting the program. **********\n";
+                break;
+            default:
+                cout << "********** Invalid choice. Please enter a valid option. **********\n";
         }
-        if(middle == 0){
-            cout<<"0";
-        }
-        cout<<swap<<endl;
-    }
+    } while (choice != 0);
 
     return 0;
 }
