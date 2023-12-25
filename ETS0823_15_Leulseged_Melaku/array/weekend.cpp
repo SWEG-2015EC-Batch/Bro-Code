c3edabac1c6819833ce25e822794efc075d77895
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-25 10:39:20 +0300

    today

--- ETS0823_15_Leulseged_Melaku/array/weekend.cpp
+++ ETS0823_15_Leulseged_Melaku/array/weekend.cpp
@@ -19,6 +19,6 @@ int main(){
 - array elements can be accessed only by it's index, the array index rages from 0 to n-1 where n is the length of the array
 - [] -to specify the size or the length of an array at the time of declaration
      -to specify the index of particular array element
-     -
+- for loop is the common looping statement that used to access navigate array elements 
 the memory address -> 0x763787347.....
 */
