4ff2dffdf9cb29bbb2182f89d431646f2944c155
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-10 08:55:58 +0300

    2

--- ETS0823_15_Leulseged_Melaku/jan-10.cpp
+++ ETS0823_15_Leulseged_Melaku/jan-10.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
+    
     float mark;
     cout<<"Eneter mark :";
     cin>>mark;
+    cout<<endl;
     cout<<"Value: "<<mark<endl;
     cout<<"Address: "<<&mark<<endl;
     cout<<"Address-2: "<<&mark+1<<endl;
     cout<<"Address-3: "<<&mark+2<<endl;
-    
+
+
     return 0;
 }
