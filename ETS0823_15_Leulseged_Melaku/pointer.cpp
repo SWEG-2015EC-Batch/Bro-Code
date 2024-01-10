27740692533eeacd5abb0aaa4a3d99d86a09413f
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-10 10:15:15 +0300

    crating a pointer of a pointer

--- ETS0823_15_Leulseged_Melaku/pointer.cpp
+++ ETS0823_15_Leulseged_Melaku/pointer.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int main(){
 
-    float ludis=2.67;*ptr;
+    float ludis=2.67,*ptr,**ptrptr;
 
     ptr = &ludis;
+    ptrptr = &ptr;// cannot store a memory of any variable except pointer; it is a pointer of a pointer
 
     cout << "Address 1: " << &ludis << endl;
     cout << "Address 2: " << ptr << endl;
-    cout << "Address 3: " << &ptr << endl;
+    cout << "Address 3: " << ptrptr << endl;
+    cout << "Address 4: " << *ptrptr << endl;
  
     cout << "Value 1: " << ludis << endl;
     cout << "Value 2: " << *ptr << endl;
+    cout << "Value 3: " << **ptrptr << endl;
 
     return 0;
 }
--- ETS0823_15_Leulseged_Melaku/tempCodeRunnerFile.cpp
+++ ETS0823_15_Leulseged_Melaku/tempCodeRunnerFile.cpp
@@ -1,1 +1,20 @@
-mark
+#include<iostream>
+using namespace std;
+int main(){
+
+    float ludis=2.67,*ptr,**ptrptr;
+
+    ptr = &ludis;
+    ptrptr = &ptr;// cannot store a memory of any variable except pointer; it is a pointer of a pointer
+
+    cout << "Address 1: " << &ludis << endl;
+    cout << "Address 2: " << ptr << endl;
+    cout << "Address 3: " << ptrptr << endl;
+    cout << "Address 4: " << *ptrptr << endl;
+ 
+    cout << "Value 1: " << ludis << endl;
+    cout << "Value 2: " << *ptr << endl;
+    cout << "Value 3: " << **ptrptr << endl;
+
+    return 0;
+}
