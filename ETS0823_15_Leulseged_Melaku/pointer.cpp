4067cc7fd9b4e6ffbfecb2afc09d6f0155120b7e
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-10 09:44:42 +0300

    creating note

--- ETS0823_15_Leulseged_Melaku/pointer.cpp
+++ ETS0823_15_Leulseged_Melaku/pointer.cpp
@@ -6,3 +6,16 @@ int main(){
 
     return 0;
 }
+
+// * dereference opreator
+/*
+    - for creating a pointer
+    - for indirectly accessing
+*/
+
+
+// address of opreator
+/*
+    - to get an address of a particular address of memory
+    - declare a reference variable
+*/
