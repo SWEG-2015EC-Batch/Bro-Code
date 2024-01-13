249851745d66f6fdb730fbf118541364fcdd58e5
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-13 15:56:23 +0300

    program skeleton

---
+++ ETS0823_15_Leulseged_Melaku/Activity 4.1/1D Array Practical Exercises/Q2.cpp
@@ -0,0 +1,7 @@
+// Write a program to input eight integer numbers into an array named temp. As each number is input, add  the number into a total. After all numbers are input, display the number and their average. 
+#include<iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
