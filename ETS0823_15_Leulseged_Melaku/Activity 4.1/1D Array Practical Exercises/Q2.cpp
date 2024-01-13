ad19b7f17cdf8f1c0b085964cdd3b20a371fc42b
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-13 16:06:26 +0300

    finishing Q2

--- ETS0823_15_Leulseged_Melaku/Activity 4.1/1D Array Practical Exercises/Q2.cpp
+++ ETS0823_15_Leulseged_Melaku/Activity 4.1/1D Array Practical Exercises/Q2.cpp
@@ -2,6 +2,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int temp[8];
+    int total = 0;
+
+    //accepting the numbers
+    for(int i = 1; i <= 8; i++){
+        cout<<"Enter number "<<i<<" ->";
+        cin>>temp[i-1];
+        total += temp[i-1];
+    }
+
+    //printing the numbers
+    cout<<"The inserted numbers are:\n\t";
+    for(int i = 0; i < 8; i++){
+        cout<<temp[i]<<", ";
+    }
+    cout<<"\nAnd their total sum is :"<<total;
+    cout<<"\nAnd their average is :"<<total/8<<endl;
+
     return 0;
 }
