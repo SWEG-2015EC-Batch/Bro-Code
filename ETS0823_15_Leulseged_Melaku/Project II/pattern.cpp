60cec8e72ddce657cdb2b3c475fcbffbf6102a4d
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-20 10:31:07 -0500

    finishing the spaces

--- ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
+++ ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
@@ -48,12 +48,33 @@ int main(){
 
     // bottom half
     for (int i = n-1; i > 0; i--){
-        for (int j = n-1; j >= 0; j--){
-            if (j < i){
-                cout<<j<<" ";
+        // for (int j = n-1; j >= 0; j--){
+        //     if (j < i){
+        //         cout<<j<<" ";
+        //     }
+        // }
+
+
+        // for (int j = 0; j <= n-1; j++){
+        //     if (i <= j){
+        //         cout<<"  ";
+        //     }
+        // }
+        // i = 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+        i--; // i = 8, 7, 6, 5, 4, 3, 2, 1, 0, -1
+        for (int j = 1; j <= n-2; j++){
+            if (i == 0){
+                break;
             }
+            if (i <= j){
+                cout<<"  ";
+            }
+
         }
-        cout<<endl;
+        if (i != 0){
+            i++;
+            cout<<endl;
+        }
     }
     return 0;
 }
