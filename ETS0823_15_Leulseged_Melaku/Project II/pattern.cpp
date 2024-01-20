f3e00a0099e505e400ebb8679703dec5071f25f7
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-20 10:50:41 -0500

    uncommention the top

--- ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
+++ ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
@@ -7,43 +7,43 @@ int main(){
     // max = n * 2 + 1
 
 
-    // // top half
-    // for (int i = 0; i < n-1; i++){
-    //     for (int j = n-2; j >= 0; j--){
-    //         if (j <= i){
-    //             cout<<j<<" ";
-    //         }
-    //     }
+    // top half
+    for (int i = 0; i < n-1; i++){
+        for (int j = n-2; j >= 0; j--){
+            if (j <= i){
+                cout<<j<<" ";
+            }
+        }
 
 
-    //     for (int j = n-2; j >= i; j--){
-    //         cout<<"  ";
-    //     }
+        for (int j = n-2; j >= i; j--){
+            cout<<"  ";
+        }
 
 
-    //     for (int j = n-3; j >= i; j--){
-    //         cout<<"  ";
-    //     }
+        for (int j = n-3; j >= i; j--){
+            cout<<"  ";
+        }
 
 
-    //     for (int j = 0; j <= i; j++){
-    //         cout<<j<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+        for (int j = 0; j <= i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
 
 
-    // // equator
-    // for(int i = n-1; i >= 0; i--){
-    //     cout<<i<<" ";
-    // }
+    // equator
+    for(int i = n-1; i >= 0; i--){
+        cout<<i<<" ";
+    }
 
 
-    // for(int i = 1; i <= n-1; i++){
-    //     cout<<i<<" ";
-    // }
+    for(int i = 1; i <= n-1; i++){
+        cout<<i<<" ";
+    }
 
-    // cout<<endl;
+    cout<<endl;
 
 
     // bottom half
