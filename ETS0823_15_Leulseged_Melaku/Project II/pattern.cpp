af7eb82d6b9a217283d2ea39e46dc87401b08bc9
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-20 09:50:07 -0500

    finishing the equator

--- ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
+++ ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
@@ -11,26 +11,42 @@ int main(){
     for (int i = 0; i < n-1; i++){
         for (int j = n-2; j >= 0; j--){
             if (j <= i){
-                cout<<j;
+                cout<<j<<" ";
             }
         }
 
 
         for (int j = n-2; j >= i; j--){
-            cout<<" ";
+            cout<<"  ";
         }
 
 
         for (int j = n-3; j >= i; j--){
-            cout<<" ";
+            cout<<"  ";
         }
 
 
         for (int j = 0; j <= i; j++){
-            cout<<j;
+            cout<<j<<" ";
         }
         cout<<endl;
     }
+
+
+    // equator
+    for(int i = n-1; i >= 0; i--){
+        cout<<i<<" ";
+    }
+
+
+    for(int i = 1; i <= n-1; i++){
+        cout<<i<<" ";
+    }
+
+    cout<<endl;
+
+
+    // bottom half
     return 0;
 }
 
