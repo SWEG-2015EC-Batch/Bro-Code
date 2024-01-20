a22f785b4f856caf15fbcc0a6543435a038c04a6
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-20 10:49:13 -0500

    editing the space in the bottom

--- ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
+++ ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
@@ -48,32 +48,26 @@ int main(){
 
     // bottom half
     for (int i = n-1; i > 0; i--){
-        // for (int j = n-1; j >= 0; j--){
-        //     if (j < i){
-        //         cout<<j<<" ";
-        //     }
-        // }
+        for (int j = n-1; j >= 0; j--){
+            if (j < i){
+                cout<<j<<" ";
+            }
+        }
 
 
-        // for (int j = 0; j <= n-1; j++){
-        //     if (i <= j){
-        //         cout<<"  ";
-        //     }
-        // }
-        // i = 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
-        // i--; // i = 8, 7, 6, 5, 4, 3, 2, 1, 0, -1
-        // for (int j = 1; j <= n-2; j++){
-        //     if (i == 0){
-        //         break;
-        //     }
-        //     if (i <= j){
-        //         cout<<"  ";
-        //     }
+        for (int j = 0; j <= n-1; j++){
+            if (i <= j){
+                cout<<"  ";
+            }
+        }
 
-        // }
-        // if (i != 0){
-        //     i++;
-        // }
+
+        for (int j = 1; j <= n-2; j++){
+            if (i <= j){
+                cout<<"  ";
+            }
+
+        }
 
 
         for (int j = 0; j <= n-2; j++){
@@ -83,9 +77,9 @@ int main(){
         }
 
         
-        if (i != 0){
-            cout<<endl;
-        }
+        
+        cout<<endl;
+        
     }
     return 0;
 }
