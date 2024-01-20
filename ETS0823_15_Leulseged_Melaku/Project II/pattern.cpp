aa91f71995741b3ee9007ad96ee3a9e323255b04
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-20 09:32:59 -0500

    project 2

---
+++ ETS0823_15_Leulseged_Melaku/Project II/pattern.cpp
@@ -0,0 +1,30 @@
+// 
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int n = 10;
+    // max = n * 2 + 1
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+//         for (int j = n-1; j >= 0; j--){
+//             if (j <= i){
+//                 cout<<j;
+//             }
+//         }
+
+
+
+
+        // for (int j = 0; j <= i; j++){
+        //     cout<<j;
+        // }
--- skeleton.cpp
+++ skeleton.cpp
@@ -2,9 +2,5 @@
 #include<iostream>
 using namespace std;
 int main(){
-    float a = 2.3;
-    float *b = &a;
-    *b = 4.3;
-    cout<<*b<<" "<<a;
-    return 0;
+    
 }
