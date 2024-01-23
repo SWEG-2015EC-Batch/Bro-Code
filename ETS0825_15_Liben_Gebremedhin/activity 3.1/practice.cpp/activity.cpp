375a5efbad801a1ad70952ece4b4e48fd093b2a7
Liben Gebremedhin <gebremedhinliben@gmail.com>
2024-01-22 22:31:50 -0800

    2'nd one

--- ETS0825_15_Liben_Gebremedhin/activity 3.1/practice.cpp/activity.cpp
+++ ETS0825_15_Liben_Gebremedhin/activity 3.1/practice.cpp/activity.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 
 int main(){
-    int array[4][5] = {
-        {16, 22, 99, 4, 18},
-        {-258, 4,101, 5, 98},
-        {105, 6, 15, 2, 45},
-        {33, 88, 72, 16, 3}
-    };
-    
-    for(int i = 0; i < 4; ++i){
-        for(int j = 0; j < 5; ++j){
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
+    int temp[8] = {};
+    int total = 0;
+    for(int i = 0; i < 8; ++i){
+        cout << "Enter a number: ";
+        cin >> temp[i];
+        total += temp[i];
     }
+    for(int i = 0; i < 8; ++i){
+        cout << temp[i] << " ";
+    }
+    cout << "\nTotal = " << total;
+
     return 0;
 }
