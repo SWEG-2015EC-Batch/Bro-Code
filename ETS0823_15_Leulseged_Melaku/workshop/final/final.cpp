9438ee95263a4ccf2738df751c448556326877c5
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-02-01 21:04:49 +0300

    new

--- ETS0823_15_Leulseged_Melaku/workshop/final/final.cpp
+++ ETS0823_15_Leulseged_Melaku/workshop/final/final.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #define LU 'l'
 using namespace std;
-int main() {
- int a[100];
 
- a[0] = 9;
- cout<<a[0];
+
+int main() {
+
+// cout<<sizeof(char)<<endl;
+// cout<<sizeof(signed char)<<endl;
+// cout<<sizeof(unsigned char)<<endl<<endl;
+
+// cout<<sizeof(short)<<endl;
+// cout<<sizeof(unsigned long int)<<endl;
+
+// cout<<sizeof(long)<<endl;
+// cout<<sizeof(signed short int)<<endl;
+
+// cout<<sizeof(unsigned int)<<endl;
+// cout<<sizeof(signed int)<<endl<<endl;
+
+// cout<<sizeof(long double)<<endl;
+// cout<<sizeof(double)<<endl;
+// cout<<sizeof(float)<<endl<<endl;
+
+// cout<<sizeof(bool)<<endl;
+// cout<<sizeof(void)<<endl;
+
+// unsigned char a = 'a';
+// cout<<a;
+
+// enum Color{MOnday = 1,teusday};
+// 
+// 
+// cout<<Color(1);
+
+
+
 
 return 0;
 }
